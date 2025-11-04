e5562fabd1dec08b3cdfe2d1ab2487107bf7a695
Konstantinos Filios <kfilios@novibet.com>
2025-11-04 15:37:11 +0200

    add dependency in packages

--- Package.swift
+++ Package.swift
@@ -1,10 +1,16 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SignalRClient",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -13,17 +19,19 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SignalRClient",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Atomics", package: "swift-atomics")
+            ]),
         .testTarget(
             name: "SignalRClientTests",
             dependencies: ["SignalRClient"]),
     ],
-    swiftLanguageVersions: [.v4, .v4_2, .v5]
+    swiftLanguageVersions: [.v5]
 )
